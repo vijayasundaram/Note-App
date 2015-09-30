2bb489a0ecab4f4a7064b7459ce64987c9df6635
vijayasundaram <vijay.sundaram@a-cti.com>
2015-09-30 21:54:42 +0530

    added google authentication

--- src/main/java/com/vijacdblz/note/controller/HomeController.java
+++ src/main/java/com/vijacdblz/note/controller/HomeController.java
@@ -1,8 +1,13 @@
-package com.vijacdblz.note;
+package com.vijacdblz.note.controller;
 
+import java.io.IOException;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
+import java.util.Map;
+
+import com.vijacdblz.note.utility.GoogleAuthHelper;
+import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,6 +15,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
 
 /**
  * Handles requests for the application home page.
@@ -23,17 +29,31 @@ public class HomeController {
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
+	public String home(Locale locale, Model model,HttpServletRequest request) {
 		logger.info("Welcome home! The client locale is {}.", locale);
+		String authcode = request.getParameter("code");
+		GoogleAuthHelper gh = new GoogleAuthHelper();
+		try {
+			
+			 Map<String, String> map = gh.getUserInfoJson(authcode);
+			 model.addAttribute(map);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		
+		
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
 		String formattedDate = dateFormat.format(date);
-		
 		model.addAttribute("serverTime", formattedDate );
 		
 		return "home";
 	}
+	
+	
+
+	
 	
 }
